ead13392792f7874e3e4af4cfdda504c1ad7329f
김현인 <kimhyuninn1018@gmail.com>
2020-09-12 02:30:36 +0900

    firstCommit

---
+++ Covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by 김현인 on 2020/09/12.
+//  Copyright © 2020 HyunInKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
