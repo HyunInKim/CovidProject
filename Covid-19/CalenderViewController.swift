7029d83e40a50e47d872cdd29d6b50efcd466981
김현인 <kimhyuninn1018@gmail.com>
2020-09-17 23:58:34 +0900

    - 기간선택 , 초기 버튼 선택 코드 수정중

--- Covid-19/CalenderViewController.swift
+++ Covid-19/CalenderViewController.swift
@@ -8,9 +8,26 @@
 
 import UIKit
 import FSCalendar
-class CalenderViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
+class CalenderViewController: UIViewController {
 
     @IBOutlet weak var calendarView: FSCalendar!
+    @IBOutlet weak var dateSelectButton: UIButton!
+    
+    let today = NSDate()
+    let todays = Date()
+    let dateFormatter = DateFormatter()
+    let calendar = Calendar.current
+    private var firstDate: Date?
+    private var lastDate: Date?
+    private var datesRange: [Date]?
+    var somedays : Array = [String]()
+    
+    fileprivate let gregorian: Calendar = Calendar(identifier: .gregorian)
+    fileprivate lazy var dateFormatter1: DateFormatter = {
+           let formatter = DateFormatter()
+           formatter.dateFormat = "yyyy-MM-dd"
+           return formatter
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         calendarView.allowsMultipleSelection = true
@@ -18,25 +35,134 @@ class CalenderViewController: UIViewController, FSCalendarDataSource, FSCalendar
         
         calendarView.delegate = self
         calendarView.dataSource = self
+        
         // Do any additional setup after loading the view.
+        calendarView.today = nil
+//        dateFormatter.dateFormat = "yyyy-MM-dd"
+        //setDefaultDate()
+        
+       
+        
     }
     
+    private func setDefaultDate() {
+        dateFormatter.dateFormat = "YYYY년 MM월 dd일"
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        let todayDate = dateFormatter.string(from: today as Date)
+        let beforeDayCurrent = calendar.date(byAdding: .day, value: -7, to: today as Date)
+        let beforeDay = dateFormatter.string(from: beforeDayCurrent!)
+        dateSelectButton.setTitle("\(beforeDay) - \(todayDate) " , for: .normal)
+      
+        
+    }
+    
+    func datesRange(from: Date, to: Date) -> [Date] {
+        // in case of the "from" date is more than "to" date,
+        // it should returns an empty array:
+        if from > to { return [Date]() }
+
+        var tempDate = from
+        var array = [tempDate]
+
+        while tempDate < to {
+            tempDate = Calendar.current.date(byAdding: .day, value: 1, to: tempDate)!
+            array.append(tempDate)
+        }
+
+        return array
+    }
+
+    
+    @IBAction func dateSelectButtonOnclick(_ sender: Any) {
+        
+    }
+    
+  
+    
+    
+
+}
+extension CalenderViewController: FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
+//    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleSelectionColorFor date: Date) -> UIColor? {
+//        somedays = ["2020-09-16",
+//                    "2020-09-17"]
+//        let dateString: String = dateFormatter1.string(from: date)
+//        if self.somedays.contains(dateString){
+//            return UIColor.green
+//        }else {
+//            return nil
+//        }
+//    }
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
+        somedays = ["2020-09-16",
+                    "2020-09-17"]
+        let dateString: String = dateFormatter1.string(from: date)
+        if self.somedays.contains(dateString){
+            return UIColor.green
+        }else {
+            return nil
+        }
+    }
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "YYYY-MM-dd"
-        print(dateFormatter.string(from: date))
+        // nothing selected:
+        if firstDate == nil {
+            firstDate = date
+            datesRange = [firstDate!]
+            return
+        }
+
+        // only first date is selected:
+        if firstDate != nil && lastDate == nil {
+            // handle the case of if the last date is less than the first date:
+            if date <= firstDate! {
+                calendar.deselect(firstDate!)
+                firstDate = date
+                datesRange = [firstDate!]
+
+                print("datesRange contains: \(datesRange!)")
+
+                return
+            }
+
+            let range = datesRange(from: firstDate!, to: date)
+
+            lastDate = range.last
+
+            for d in range {
+                calendar.select(d)
+            }
+
+            datesRange = range
+
+            print("datesRange contains: \(datesRange!)")
+
+            return
+        }
+
+        // both are selected:
+        if firstDate != nil && lastDate != nil {
+            for d in calendar.selectedDates {
+                calendar.deselect(d)
+            }
+
+            lastDate = nil
+            firstDate = nil
+
+            datesRange = []
+
+            print("datesRange contains: \(datesRange!)")
+        }
     }
-    
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        // both are selected:
+        if firstDate != nil && lastDate != nil {
+            for d in calendar.selectedDates {
+                calendar.deselect(d)
+            }
+            lastDate = nil
+            firstDate = nil
+            datesRange = []
+        }
     }
-    */
-
 }
